7f260f1d3ceca243f733904821acf291ba0a68fe
Tawfiek <>
2022-01-21 18:18:17 +0200

    issue: remove extra dot in index file name

--- dist/routes/apis/list-names.js
+++ dist/routes/apis/list-names.js
@@ -1,1 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+exports.default = router;
 //# sourceMappingURL=list-names.js.map
